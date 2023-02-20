fa77777968534c8a42979a3a6da298e6280ef864
Jenny Carroll <jennygc@telus.net>
2023-02-20 20:37:01 +0000

    add an additional endpoint /urs.json

--- express_server.js
+++ express_server.js
@@ -6,11 +6,19 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
+//here is one endpoint
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+//add an additional endpoint
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+// Start the server by running node express_server.js in your terminal
+// Visit http://localhost:8080/ in your browser and make sure you can see the Hello! response.
