018421cdcb2ac9b4adb7ebdfefd4fa9c537454c5
Jenny Carroll <jennygc@telus.net>
2023-02-20 23:49:39 +0000

    create views folder and urls_index.ejs and copy over HTML code that has javascript inside to display a list of URLs in their shortened form and a link for shortening a URL

--- express_server.js
+++ express_server.js
@@ -1,7 +1,12 @@
+// Using EJS and HTML, code the urls_index.ejs file to display the following:
+// A list or table of the URLs and their shortened forms. If you need a reminder on how to loop through data with EJS, take another peek at the What is a Template Engine? Reading.
+// A link for shortening a new URL. This will be a dead link for now (href='#') as we will build the page for this functionality later.
+
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+//This tells the Express app to use EJS as its templating engine.
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -18,6 +23,12 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//route handler for /urls that renders using our template and template variable object
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
 //the response can contain HTML code, which would be rendered in the client browser.
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -29,3 +40,7 @@ app.listen(PORT, () => {
 
 // Start the server by running node express_server.js in your terminal
 // Visit http://localhost:8080/ in your browser and make sure you can see the Hello! response.
+// Notice the external link we've included in the <head> tag; this stylesheet is from Bootstrap,
+// one of the most popular CSS frameworks around. We'll use it in our app so that we have some basic CSS
+// styling available to us. We've also included a number of <script> tags at the bottom of our <body>;
+// these javascript files are required by some of the components in Bootstrap.
