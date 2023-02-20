0c6184ebbd9110708e3fb2910747104f0fc5c725
Jenny Carroll <jennygc@telus.net>
2023-02-20 20:46:38 +0000

    add a response that contains HTML code

--- express_server.js
+++ express_server.js
@@ -16,6 +16,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//the response can contain HTML code, which would be rendered in the client browser.
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
